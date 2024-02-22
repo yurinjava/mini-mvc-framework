24aebc0c4f234c892e3e713966ffe83956d3977f
yurinjava <yurisouzact@gmail.com>
2024-02-22 17:48:07 -0300

    layouts

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -1,29 +1,23 @@
 <?php
 
  namespace App\Controllers;
-     
-    class IndexController{
-        private $view;
-        public function __construct(){
-            $this->view = new \stdClass();
-        }
+
+ use MF\Controller\Action;
+
+    class IndexController extends Action{
+       
+       
         public function index(){
             //echo "estamos no indexcontroller e disparamos a action index";
             $this->view->dados = array('sofa','cadeira', 'cama');
-            $this->render('index');
+            $this->render('index', 'layout1');
         }
         public function sobreNos(){
             //echo "estamos no sobrenos e disparamos a action sobreNos";
             $this->view->dados = array('notebook','desktop', 'tablet');
-            $this->render('sobreNos');
+            $this->render('sobreNos', 'layout2');
         }
-        public function render($view){
-            $classAtual = get_class($this);
-            $classAtual = str_replace('App\\Controllers\\','', $classAtual);
-            $classAtual= strtolower(str_replace('Controller', '', $classAtual));
-
-            require_once "../App/Views/".$classAtual."/".$view.".phtml";
-        }
+       
     }
 
 ?>
